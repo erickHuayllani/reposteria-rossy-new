845e101900b3cacf82c5d2241655e75f218cd011
Erick Huayllani <74062867@continental.edu.pe>
2025-11-30 22:05:54 -0500

    fix: add vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
+import vercel from '@astrojs/vercel'; // ← ESTA LÍNEA ERA LO QUE FALTABA
 
 // https://astro.build/config
 export default defineConfig({
