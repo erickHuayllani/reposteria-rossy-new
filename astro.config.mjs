82244944198b36ec9faa743e0275b52bfab6fad8
Erick Huayllani <74062867@continental.edu.pe>
2025-12-02 18:24:05 -0500

    Actualizando proyecto completo

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-import react from '@astrojs/react';
-import vercel from '@astrojs/vercel/server';
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import react from "@astrojs/react";
+import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
-  output: 'server',
+  output: "server",
   adapter: vercel(),
 });
